ae262a97662e4b459fff848eefaba07da5ddcf1d
Jordan Coyle <engineerofsoftware@gmail.com>
2015-02-12 02:51:35 +0000

    Started camera script

---
+++ IntegratedProject2/Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraFollow : MonoBehaviour {
+
+	public Transform pla;
+
+	// Use this for initialization
+	void Start () {
+
+		//pla = GameObject.Find("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(player.position.x + 6, 0, -10);
+	
+	}
+}
