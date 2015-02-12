0e2e62f697a263429f152908ce82e914d3372ee7
Jordan Coyle <engineerofsoftware@gmail.com>
2015-02-12 00:43:37 +0000

    Basic movement completed

--- IntegratedProject2/Assets/Scripts/Movement.cs
+++ IntegratedProject2/Assets/Scripts/Movement.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	public float speed = 7.0f;
-	public float jumpForce = 20.0f;
-	private bool grounded = true;
+	public float jumpForce = 200.0f;
+	public Transform groundCheck;
+	public LayerMask groundMask;
+	private float groundRadius = 0.3f;
+	private bool grounded = false;
 	//private Touch touch;
 
 	// Use this for initialization
@@ -30,23 +33,29 @@ public class Movement : MonoBehaviour {
 		}
 	}*/
 
-	// Update is called once per frame
-	void FixedUpdate () {
-
-		//moves the player in a straight line
-		rigidbody2D.velocity = new Vector2(speed, 0.0f);
-
+	void Update()
+	{
 		//checks button input
 		if(Input.GetButtonDown("Jump"))
 		{
 			//checks if player is grounded before jumping
 			if(grounded)
 			{
-			    rigidbody2D.AddForce (new Vector2(0.0f, jumpForce));
-				grounded = true;
+				grounded = false;
+				rigidbody2D.AddForce (new Vector2(0.0f, jumpForce));
 			}
-
+			
 		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//check if groundcheck object touches collider
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundMask);
+
+		//moves the player in a straight line
+		rigidbody2D.AddForce (new Vector2(speed, 0.0f));
 
 	
 	}
