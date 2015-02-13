1f33167d9f8e9bad1272bdfc4ef2128fc1013dc3
Jordan Coyle <engineerofsoftware@gmail.com>
2015-02-13 00:08:53 +0000

    Created random spawn script

---
+++ IntegratedProject2/Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject[] items;
+	public float minSpawn = 1.0f;
+	public float maxSpawn = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		SpawnBehaviour();
+	
+	}
+
+	void SpawnBehaviour()
+	{
+		Instantiate(items[Random.Range (0, items.Length)], transform.position, Quaternion.identity);
+		Invoke("SpawnBehaviour", Random.Range (minSpawn, maxSpawn));
+	}
+
+}
