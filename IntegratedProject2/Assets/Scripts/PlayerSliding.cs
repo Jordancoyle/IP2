921e65e1bd23cc9334479daaef37c13d8838e7df
Jordan Coyle <engineerofsoftware@gmail.com>
2015-02-26 00:30:07 +0000

    Base slide and project and scene changes
    Primitive slide mechanic completed and changes where made to the
    collision matrix. Changes where also made to the scene to test new
    mechanic

---
+++ IntegratedProject2/Assets/Scripts/PlayerSliding.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSliding : MonoBehaviour {
+
+	public float slideFriction = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButton("Slide"))
+		{
+			gameObject.layer = 10;
+			rigidbody2D.AddForce(new Vector2(-slideFriction, 0.0f));
+		}
+		else{
+			gameObject.layer = 0;
+		}
+
+	
+	}
+}
