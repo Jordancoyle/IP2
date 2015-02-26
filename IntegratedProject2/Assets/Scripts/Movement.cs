ce18c395d9f1422cce6314281a86f377bdadaa75
Jordan Coyle <engineerofsoftware@gmail.com>
2015-02-26 02:32:26 +0000

    Fixed movement
    frictionless material added to prefab

--- IntegratedProject2/Assets/Scripts/Movement.cs
+++ IntegratedProject2/Assets/Scripts/Movement.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	public float speed = 7.0f;
-	public float maxSpeed = 30.0f;
+	public float maxSpeed = 25.0f;
 	public float jumpForce = 300.0f;
-	public float altJumpForce = 10;
+	public float altJumpForce = 10.0f;
 	public float airRes = 20.0f;
 	public Transform groundCheck;
 	public LayerMask groundMask;
 	private float groundRadius = 0.2f;
-	private bool grounded = true;
+	private bool grounded;
 	//private Touch touch;
 
 	// Use this for initialization
@@ -44,9 +44,10 @@ public class Movement : MonoBehaviour {
 		if(Input.GetButtonDown("Jump"))
 		{
 			//checks if player is grounded before jumping
-			if(grounded)
+			if(grounded == true)
 			{
 				grounded = false;
+				Debug.Log ("false");
 				rigidbody2D.AddForce (new Vector2(0.0f, jumpForce));
 				//rigidbody2D.AddForce (new Vector2(-airRes, 0.0f));
 			}
