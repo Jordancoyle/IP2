ec12db2dcef94b7f29654e4e81a4b703f2c10c03
Jordan Coyle <engineerofsoftware@gmail.com>
2015-03-03 23:18:54 +0000

    Created main menu and pause menu
    created both menu with connecting scripts

---
+++ IntegratedProject2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	bool paused = false;
+
+	void Update()
+	{
+		if(Input.GetButtonDown ("Pause"))
+		{
+			if(paused == true)
+			{
+				Time.timeScale = 1.0f;
+				pauseMenu.gameObject.SetActive(false);
+				paused = false;
+			}
+
+			else{
+				Time.timeScale = 0.0f;
+				pauseMenu.gameObject.SetActive(true);
+				paused = true;
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		Time.timeScale = 1.0f;
+		pauseMenu.gameObject.SetActive(false);
+	}
+
+	public void ReturnToMainMenu()
+	{
+		Time.timeScale = 1.0f;
+		Application.LoadLevel ("MainMenuScene");
+	}
+
+}
---
+++ IntegratedProject2/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
